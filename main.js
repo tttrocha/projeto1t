6ca5149513b4a80a9c95a2dde896a9c489be26dc
tttrocha <108011467+tttrocha@users.noreply.github.com>
2024-04-29 19:39:28 -0300

    yyry

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("dias7").textContent = calculaTempo(tempos[5])[2];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
